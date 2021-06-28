4f3b866e53831397a9706736cd3f4ea107b9eaae
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-06-28 13:08:08 +0530

    Deploying to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,13 +8,16 @@ connectDB();
 
 app.use(express.json({ extented: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'hey there' });
-});
-
 //routes
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 //listen
 const PORT = 5000;
